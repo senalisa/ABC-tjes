f8acc526ec9eba807e06bac3e54f62021dd74305
Raiesa <83213316+zehra1810@users.noreply.github.com>
2022-07-01 11:24:49 +0200

    update start page sound

--- src/startpage.ts
+++ src/startpage.ts
@@ -10,8 +10,10 @@ import startbuttonImage from "./images/startbutton.png";
 import heroImage from "./images/superhero.png"
 import crownImage from "./images/crown.png"
 
-//IMPORT CLASSES
-import { Player } from "./player";
+//IMPORT SOUND
+import startschermSound from "url:./sound/startschermsound.mp3";
+import startvoiceSound from "url:./sound/startvoice.mp3";
+
 
 //GAME CLASS
 export class Game {
@@ -27,6 +29,9 @@ export class Game {
   public speed: number;
 
   public startButton: PIXI.Sprite;
+
+  public startschermSound: HTMLAudioElement
+  public startvoiceSound: HTMLAudioElement
 
   //CONSTRUCTOR
   constructor() {
@@ -48,9 +53,13 @@ export class Game {
       .add("heroTexture", heroImage)
       .add("crownTexture", crownImage)
 
+      .add("startschermSound", startschermSound)
+      .add("startvoiceSound", startvoiceSound)
+
     this.loader.load(() => this.loadCompleted());
 
-    this.speed = 3
+    //  MOUSE CURSOR TRACKER
+    document.addEventListener("mousemove", (e: MouseEvent) => this.mouseMoveHandler(e));
 
   }
 
@@ -71,6 +80,15 @@ export class Game {
     crown.x = 550
     crown.y = 60
     this.pixi.stage.addChild(crown)
+
+    //SOUND
+    this.startschermSound = this.loader.resources["startschermSound"].data!
+    this.startschermSound.volume = 0.5
+    this.startvoiceSound = this.loader.resources["startvoiceSound"].data!
+
+    //PLAY SOUND
+    this.startschermSound.play();
+    this.startvoiceSound.play();
 
     //PLAYER HERO RAIESA
     let player = new PIXI.Sprite(this.loader.resources["playerTexture"].texture!);
@@ -102,5 +120,16 @@ export class Game {
     console.log("klik")
     window.location.href = "levelpage.html"
   }
+
+      // MOUSE CURSOR
+      mouseMoveHandler(e: MouseEvent) {
+        var relativeY = e.clientY - this.pixi.screen.top
+    
+        if (relativeY > 0 && relativeY < this.pixi.screen.height) {
+          this.startschermSound.play()
+          this.startvoiceSound.play()
+    
+          
+        }
 
 }
